fdc2312602038718d3999302688df4028bd8c22d
PlebMin <elio.pinguet@isen-ouest.yncrea.fr>
2023-06-06 01:14:56 +0200

    final change working

--- pong.js
+++ pong.js
@@ -27,10 +27,17 @@ let cadreRight = document.getElementById('cadre').offsetWidth + border;
 let partieId;
 
 
-function start() {
-    moveballinter = setInterval(moveBall, 5);
-}
+/* clears the interval of the moveballinter variable
+   and then sets the interval of the moveballinter variable to 
+   the return value of the setInterval function. */
 
+function start() {
+  clearInterval(moveballinter);                     
+   moveballinter = setInterval(moveBall, 10);    
+}
+ 
+/*function that move the ball and manage the ball bounce */
+/*switchcolor() is the feature */
 function moveBall() {
   let newBallXPos = ballXPos + ballXDir;
   let newBallYPos = ballYPos + ballYDir;
@@ -42,12 +49,14 @@ function moveBall() {
     ((newBallYPos <= raquette2.offsetTop && newBallYPos+ballHeight >= raquette2.offsetTop) ||
     (newBallYPos >= raquette2.offsetTop && newBallYPos+ballHeight <= raquette2.offsetTop+raquetteHeight) ||
     (newBallYPos <= raquette2.offsetTop+raquetteHeight && newBallYPos+ballHeight >= raquette2.offsetTop+raquetteHeight))) {
+    switchcolor();
     ballXDir = -Math.abs(ballXDir);
   }
   if (newBallXPos < raquette1.offsetLeft &&
     ((newBallYPos <= raquette1.offsetTop && newBallYPos+ballHeight >= raquette1.offsetTop) ||
     (newBallYPos >= raquette1.offsetTop && newBallYPos+ballHeight <= raquette1.offsetTop+raquetteHeight) ||
     (newBallYPos <= raquette1.offsetTop+raquetteHeight && newBallYPos+ballHeight >= raquette1.offsetTop+raquetteHeight))) {
+    switchcolor();
     ballXDir = Math.abs(ballXDir);
   }
 
@@ -82,19 +91,22 @@ function moveBall() {
 }
 
 function init() {
-    ball.style.top = ballYPos + 'px';
-    ball.style.left = ballXPos + 'px';
+  ball.style.top = ballYPos + 'px';
+  ball.style.left = ballXPos + 'px';
 
-    let joueur1 = document.getElementById('j1');
-    let joueur2 = document.getElementById('j2');
+  let joueur1 = document.getElementById('j1');
+  let joueur2 = document.getElementById('j2');
 
-    remapKeys();
-    setInterval(keyboardControlGlobal, 50);
+  remapKeys();
+  setInterval(keyboardControlGlobal, 50);
     
 }
 
+/**Called to stop the game */
 function stopgame(x){
+  /**Stop the game */
   clearInterval(moveballinter);
+  /**Set the score for each player */
   let scoreJ1 = document.querySelector(`.score.J1>p`);
   let scoreJ2 = document.querySelector(`.score.J2>p`);
   if (!x){
@@ -103,23 +115,24 @@ function stopgame(x){
   else{
     scoreJ2.textContent = parseInt(scoreJ2.textContent)+1;
   }
-  
+  /**verify if the game isn't finished */
   if ((scoreJ1.textContent < 5 && scoreJ2.textContent < 5) || (Math.abs(scoreJ1.textContent-scoreJ2.textContent) < 2)){
     
 		moveballinter = setInterval(() => ballsticked(x), 13); // Set an interval to stick the ball to a player
-		return;
+		partieId = 0;
+    return;
 	}
 
   if (scoreJ1 > scoreJ2) {
-    document.getElementById("infos").innerHTML = "GAME OVER1";
+    document.getElementById("infos").innerHTML = "Joueur 1 à Gagné";
     document.getElementById("infos").style.display = "block";
-    partieId = undefined;
+    partieId = 1;
   }
     
   else{
-    document.getElementById("infos").innerHTML = "GAME OVER2";
+    document.getElementById("infos").innerHTML = "Joueur 2 à Gagné";
     document.getElementById("infos").style.display = "block";
-    partieId = undefined;
+    partieId = 1;
   }
 
 }
@@ -135,11 +148,10 @@ function ballsticked(x){
     ball.style.top = raquette1.offsetTop + (raquetteWidth/2) +25 + "px";
     ballXDir = Math.abs(ballXDir);
 	} 
-	partieId = 0;
 }
 
+// Handle start the game inputs
 function keyboardControlGlobal(){
-  
   keyPressed.forEach(key => {
     switch (key){
       case 'Space':
@@ -157,11 +169,11 @@ function keyboardControlGlobal(){
         break;
       case 'KeyQ':
            raquetteTop = raquette1.offsetTop;
-           newRaquetteTop = raquetteTop - 50;
+           newRaquetteTop = raquetteTop - 40;
           if (newRaquetteTop > cadreTop) {
               raquette1.style.top = newRaquetteTop + 'px';
               if(!istsarted){
-                ball.style.top = ball.offsetTop - 50 + 'px';
+                ball.style.top = ball.offsetTop - 40 + 'px';
                 ballYPos = ballYPos -50;
               }
             }
@@ -169,27 +181,27 @@ function keyboardControlGlobal(){
 
       case 'KeyA':
           raquetteTop = raquette1.offsetTop;
-          newRaquetteTop = raquetteTop + 50;
+          newRaquetteTop = raquetteTop + 40;
           if (newRaquetteTop < logoHeight + cadreBottom - raquette1.offsetHeight /2 -5) {
               raquette1.style.top = newRaquetteTop + 'px';
 
             if(!istsarted){
-              ball.style.top = ball.offsetTop + 50 + 'px';
-              ballYPos = ballYPos +50;
+              ball.style.top = ball.offsetTop + 40 + 'px';
+              ballYPos = ballYPos +40;
             }
           }
           break;
 
       case 'KeyP':
            raquetteTop = raquette2.offsetTop;
-           newRaquetteTop = raquetteTop - 50;
+           newRaquetteTop = raquetteTop - 40;
           if (newRaquetteTop > cadreTop) {
               raquette2.style.top = newRaquetteTop + 'px';
           }
           break;
       case 'KeyL':
           raquetteTop = raquette2.offsetTop;
-          newRaquetteTop = raquetteTop + 50;
+          newRaquetteTop = raquetteTop + 40;
           if (newRaquetteTop < logoHeight +cadreBottom - raquette2.offsetHeight /2 -5) {
               raquette2.style.top = newRaquetteTop + 'px';
           }
@@ -198,6 +210,8 @@ function keyboardControlGlobal(){
   })
 }
 
+/*Remap key press to avoid "writting behavior"
+https://stackoverflow.com/a/3691661/21143650*/
 function remapKeys(){
   // Remap key press
   document.onkeydown = e => {
@@ -216,6 +230,33 @@ function remapKeys(){
 
 }
 
-// 5 class pour couleur  suppre tt les couleur randint dans index list correspondant a la class pour couleur 
+/**Choose one the color on the old Apple logo */
+function switchcolor(){
+  ball.style.backgroundColor = "transparent";
+  /**Choose a number between 0 and 5 */
+  var randomcol = Math.floor(Math.random()*6);
+  var backgroundColor;
+  switch (randomcol) {
+    case 0:
+      backgroundColor = "#009DDC";
+      break;
+    case 1:
+      backgroundColor = "#963D97";
+      break;
+    case 2:
+      backgroundColor = "#E03A3E";
+      break;
+    case 3:
+      backgroundColor = "#F5821F";
+      break;
+    case 4:
+      backgroundColor = "#FDB827";
+      break;
+    case 5:
+      backgroundColor = "#61BB46";
+      break;
+  }
+  ball.style.backgroundColor = backgroundColor;
+}
 
 init();
